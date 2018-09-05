270d1fd0c55a39eb5bab91a3f2a82bdd60311231
Your Name <gabriel.rodriguezt1022@gmail.com>
2018-09-04 20:25:30 -0500

    Definicion de API REST, creacion de servicios para autenticacion de usuarios por jwt, creacion de controlador para servicios de imagenes

--- proyectreils/config/routes.rb
+++ proyectreils/config/routes.rb
@@ -5,5 +5,12 @@ Rails.application.routes.draw do
   resources :posts
   resources :images
   root 'home#index'
+  # API REST
+  namespace 'api' do
+    namespace 'v1' do
+      post 'auth_user' => 'authentication#authenticate_user'
+    end
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
